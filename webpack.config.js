574f9c4ec3c63d9c564a1c9fe8d5bb04df51776b
minsion <15238290258@163.com>
2024-06-21 16:38:40 +0800

    modify little

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
-const path = require('path');
-const deps = require('./package.json').dependencies;
 module.exports = {
   entry: './src/index',
   mode: 'development',
   target: 'web',
   devServer: {
     port: 3001,
+    historyApiFallback: true,
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    client: {
+      overlay: false,
+    },
     webSocketServer: false,
     // server: "spdy",
     proxy: {
